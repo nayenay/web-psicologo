f1734c7a7bffb4195b28a29e9cf9544083a5ba72
nayenay <madridberenice21@gmail.com>
2026-07-14 21:21:52 -0600

    chore: Conoceme with fake text and without carousel

--- src/components/home/Conoceme.tsx
+++ src/components/home/Conoceme.tsx
@@ -1,15 +1,31 @@
+
 import Container from "../layout/Container";
+
+
 export default function Conoceme(){
     return (
         <section id="servicios"
             className="py-20 bg-[var(--background)]">
             <Container>
                 <div className="text-center mb-16">
-                    <h2 className=" text 4x1 font-bold text-[var-primary-dark)]">
+                    <h2 className=" text-4xl font-bold text-[var(--primary-dark)]">
                         Conóceme
-                    </h2>
+                    </h2>             
                     <div className="  mx-auto  mt-5  h-1  w-28  rounded-full  bg-[var(--primary)]  "/>
-                </div >
+                </div >    
+                <div className="text-center mb-16">
+                    <h3  className="  mb-3  text-2xl  font-semibold  text-justify  text-[var(--primary-light)]  "  >
+                        ¿Quién soy?
+                    </h3>
+                    <p className="text-xl leading-9 text-justify text-[var(--primary-dark)]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
+                </div> 
+                <div className="text-center mb-16">   
+                    <h3  className="  mb-3  text-2xl  font-semibold  text-justify  text-[var(--primary-light)]  "  >
+                        Forma de trabajo
+                    </h3>
+                    <p className="text-xl leading-9 text-justify text-[var(--primary-dark)]">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. </p>
+                </div>
+                
             </Container>
         </section>
     )
