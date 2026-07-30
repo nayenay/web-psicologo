b0d92b6d00208429488da74efecad0da187b3eda
nayenay <madridberenice21@gmail.com>
2026-07-30 00:27:18 -0600

    feat: screen register is working

---
+++ src/app/register/page.tsx
@@ -0,0 +1,151 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
+
+import Container from "@/components/layout/Container";
+import Card from "@/components/forms/Card";
+import Input from "@/components/forms/Input";
+import PasswordInput from "@/components/forms/PasswordInput";
+import SubmitButton from "@/components/forms/SubmitButton";
+
+import { register } from "@/services/auth.service";
+
+
+
+
+export default function Register() {
+    const router = useRouter();
+
+    const [nombre, setNombre] = useState("");
+    const [apellidoPaterno, setApellidoPaterno] = useState("");
+    const [apellidoMaterno, setApellidoMaterno] = useState("");
+    const [correo, setCorreo] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [password, setPassword] = useState("");
+
+    //función para validar los datos en un formulario y prepararlos antes de enviarlos
+    async function handleSubmit(
+        e: React.FormEvent<HTMLFormElement>
+    ) {
+
+        e.preventDefault();
+
+        try {
+
+            await register({
+
+                nombre,
+                apellidoPaterno,
+                apellidoMaterno,
+                correo,
+                telefono: Number(telefono),
+                password,
+
+            });
+
+            router.push("/homepage");
+
+        } catch (error) {
+
+            console.error(error);
+
+            alert("No fue posible crear la cuenta.");
+
+        }
+        
+    }
+
+
+
+
+
+
+
+    return (
+        <form
+            className="space-y-5"
+            onSubmit={handleSubmit}
+        >
+
+        <Input
+            label="Nombre"
+            value={nombre}
+            onChange={(e) => setNombre(e.target.value)}
+        />
+
+        <Input
+            label="Primer apellido"
+            value={apellidoPaterno}
+            onChange={(e) =>
+                setApellidoPaterno(e.target.value)
+            }
+        />
+
+        <Input
+            label="Segundo apellido (opcional)"
+            value={apellidoMaterno}
+            onChange={(e) =>
+                setApellidoMaterno(e.target.value)
+            }
+        />
+
+        <Input
+            label="Correo electrónico"
+            type="email"
+            value={correo}
+            onChange={(e) =>
+                setCorreo(e.target.value)
+            }
+        />
+
+        <Input
+            label="Teléfono"
+            type="tel"
+            value={telefono}
+            onChange={(e) =>
+                setTelefono(e.target.value)
+            }
+        />
+
+        <PasswordInput
+
+            value={password}
+
+            onChange={(e) =>
+                setPassword(e.target.value)
+            }
+
+        />
+
+        <SubmitButton>
+
+            Crear cuenta
+
+        </SubmitButton>
+
+        <div className="mt-8 text-center">
+
+            <p className="text-sm">
+
+                ¿Ya tienes cuenta?
+
+            </p>
+
+            <Link
+                href="/login"
+                className="text-[var(--accent)] hover:underline"
+            >
+
+                Inicia sesión
+
+            </Link>
+
+        </div>
+
+        </form>
+    );
+}
+
