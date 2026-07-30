f3e13f9e875bb60e0da7c8d4ea919dff15ce7e8d
nayenay <madridberenice21@gmail.com>
2026-07-29 19:29:31 -0600

    chore: Protected Route

---
+++ src/auth/ProtectedRoute.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import { ReactNode, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import useAuth from "@/hooks/useAuth";
+
+interface ProtectedRouteProps {
+    children: ReactNode;
+    roles: string[];
+}
+
+export default function ProtectedRoute({
+    children, //recibe /dashboard ej
+    roles, //recibe rol
+}: ProtectedRouteProps) {
+    //acceso al contexto
+    const router = useRouter();
+
+    const { user, loading } = useAuth();
+    //crear useEfect
+    useEffect(() => {
+
+        if (loading) return;//esperar a que firebase termine
+
+        if (!user) {//verificar si existe el usuario
+            router.replace("/login");
+            return;
+        }
+
+        if (!user.activo) {//verificar que el usuario no está activo
+            router.replace("/login");
+            return;
+        }
+
+        if (!roles.includes(user.rol)) {//verificar si el rol coinside
+            router.replace("/");//regresar
+            return;
+        }
+
+    }, [loading, user, roles, router]);
+
+    if (loading || !user) {
+        return null;//después puedo mostrar un spinner
+    }
+
+    return <>{children}</>;
+}
